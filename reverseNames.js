cdbf2671112871e18dce869d283189d60a2cc2c5
mzakocs <mzakocs@gmail.com>
2020-09-25 09:24:42 -0700

    Fixed a bug where the names would be re-formatted repeatedly

--- reverseNames.js
+++ reverseNames.js
@@ -1,18 +1,23 @@
+var alreadyDone = false;
 const checkForNames = () => {
   // Grabs all of the name elements with the class name "cS7aqe NkoVdd"
   let nameDivs = document.getElementsByClassName("cS7aqe NkoVdd");
   // Checks the length of the collection
   if (nameDivs.length > 0) {
     // Reverses the names
-    for (let i = 0; i < nameDivs.length; i++) {
-      let name = nameDivs[i].innerText;
-      let splitName = name.split(" ");
-      if (splitName.length == 2) {
-        nameDivs[i].innerText = splitName[1] + ", " + splitName[0];
+    if (!alreadyDone) {
+      alreadyDone = true;
+      for (let i = 0; i < nameDivs.length; i++) {
+        let name = nameDivs[i].innerText;
+        let splitName = name.split(" ");
+        if (splitName.length == 2) {
+          nameDivs[i].innerText = splitName[1] + ", " + splitName[0];
+        } else continue;
       }
-      else continue;
     }
+  } else {
+    alreadyDone = false;
   }
-}
+};
 
 setInterval(checkForNames, 2000);
