c0621d59b1978749d2847e5bc299e62ff57a0ae9
mzakocs <mzakocs@gmail.com>
2020-09-25 08:45:53 -0700

    Added injection script

---
+++ reverseNames.js
@@ -0,0 +1,18 @@
+const checkForNames = () => {
+  // Grabs all of the name elements with the class name "cS7aqe NkoVdd"
+  let nameDivs = document.getElementsByClassName("cS7aqe NkoVdd");
+  // Checks the length of the collection
+  if (nameDivs.length > 0) {
+    // Reverses the names
+    for (let i = 0; i < nameDivs.length; i++) {
+      let name = nameDivs[i].innerText;
+      let splitName = name.split(" ");
+      if (splitName.length == 2) {
+        nameDivs[i].innerText = splitName[1] + ", " + splitName[0];
+      }
+      else continue;
+    }
+  }
+}
+
+setInterval(checkForNames, 2000);
