ac645056ac0d0027d56d90da5bf08389597290f7
mzakocs <mzakocs@gmail.com>
2020-10-13 09:44:45 -0700

    Updated to update the name list when people come into the call

--- reverseNames.js
+++ reverseNames.js
@@ -1,23 +1,20 @@
-var alreadyDone = false;
 const checkForNames = () => {
   // Grabs all of the name elements with the class name "cS7aqe NkoVdd"
   let nameDivs = document.getElementsByClassName("cS7aqe NkoVdd");
   // Checks the length of the collection
   if (nameDivs.length > 0) {
     // Reverses the names
-    if (!alreadyDone) {
-      alreadyDone = true;
-      for (let i = 0; i < nameDivs.length; i++) {
-        let name = nameDivs[i].innerText;
+    alreadyDone = true;
+    for (let i = 0; i < nameDivs.length; i++) {
+      let name = nameDivs[i].innerText;
+      if (!name.includes(',')) {
         let splitName = name.split(" ");
         if (splitName.length == 2) {
           nameDivs[i].innerText = splitName[1] + ", " + splitName[0];
         } else continue;
       }
     }
-  } else {
-    alreadyDone = false;
   }
 };
 
-setInterval(checkForNames, 2000);
+setInterval(checkForNames, 1500);
