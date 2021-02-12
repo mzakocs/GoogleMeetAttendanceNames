b821f65873cd02e3046821aa1f8d79a642ff7fe8
mzakocs <mzakocs@gmail.com>
2021-02-12 14:56:33 -0700

    Added last name sorting feature

--- reverseNames.js
+++ reverseNames.js
@@ -2,10 +2,13 @@ const checkForNames = () => {
   // get the Meeting details > Participants container node
   const participantsContainer = document.querySelector('[aria-label="Participants"]');
   
+  // Ends if the participantsContainer doesn't exist
+  if (!participantsContainer) return;
+
   // convert the HTMLCollection to an iterable
   const participantsElements = Array.from(participantsContainer.children ?? []);
 
-  participantsElements.forEach((participantElement) => {
+  participantsElements.forEach((participantElement, i) => {
     // find the img element as it should be the previousSibling to the name container
     const [imageElement] = participantElement.getElementsByTagName('img');
     const nameContainer = imageElement?.nextSibling;
@@ -19,6 +22,26 @@ const checkForNames = () => {
         if (!nameElement.textContent.includes(',')) {
           const [fName, lName, ...names] = nameElement.textContent.split(' ');
           nameElement.textContent = `${lName}${names.length ? ' ' + names.join(' ') : ''}, ${fName}`;
+          // Sorts the array of students by last name on the final iteration
+          if (i == (participantElement.length - 1)) {
+            participantsElements.sort(function(a, b) {
+              var nameA = a.children[0].children[1].children[0].innerText.split(",")[0].toUpperCase(); // grabs last name from child div
+              var nameB = b.children[0].children[1].children[0].innerText.split(",")[0].toUpperCase(); // grabs last name from child div
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              return 0;
+            });
+          }
+          // Removes elements to replace them in sorted order
+          // This is really janky but it works 
+          participantsElements.forEach(function(element) {
+            element.remove();
+            participantsContainer.append(element);
+          });
         } 
       }
     }
